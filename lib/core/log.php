35c137fdbb607e191ba82fabbd7e2043834af060
Zheng Kai <zhengkai@gmail.com>
2014-06-10 17:45:07 +0800

    log file writable check

--- lib/core/log.php
+++ lib/core/log.php
@@ -80,7 +80,7 @@ class Log {
 			return FALSE;
 		}
 
-		if (file_exists($sFile) && filesize($sFile) > $aConfig['max_size']) {
+		if (file_exists($sFile) && (filesize($sFile) > $aConfig['max_size'] || !is_writable($sFile))) {
 			return FALSE;
 		}
 
