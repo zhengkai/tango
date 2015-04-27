35a6d0c4ab02df03be80e7febcf21d540d2a93a0
Zheng Kai <zhengkai@gmail.com>
2015-04-27 16:46:41 +0800

    up make phpdoc script

--- make_phpdoc.sh
+++ make_phpdoc.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-$(dirname `readlink -f $0`)/vendor/bin/phpdoc
+cd $(dirname `readlink -f $0`)
+phpdoc_bin='./vendor/bin/phpdoc'
+doc_dir='./doc'
+
+if [ ! -f "$phpdoc_bin" ]; then
+	echo 'no phpdoc, file = '$phpdoc_bin
+	exit
+fi
+
+if [ ! -d "$doc_dir" ]; then
+	echo 'no doc, dir = '$doc_dir
+	exit
+fi
+
+\rm -r $doc_dir/*
+$phpdoc_bin
