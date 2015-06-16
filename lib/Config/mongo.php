eef8cee153f9a5e57c2c86fb07cff2672b191a69
Zheng Kai <zhengkai@gmail.com>
2015-06-16 15:06:48 +0800

    mongo 没写完，先放在那了

---
+++ lib/Config/mongo.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'default' => [
+		'server' => 'mongodb://localhost:27017',
+		'debug' => FALSE,
+	],
+	'server' => [
+		'tango_conf' => [
+			'db' => 'tango_db',
+			'collection' => 'tango_coll',
+		],
+	],
+];
--- lib/Drive/Mongo.php
+++ lib/Drive/Mongo.php
@@ -10,6 +10,11 @@
 
 namespace Tango\Drive;
 
+use Tango\Core\Config;
+use Tango\Core\TangoException;
+
+Config::setFileDefault('mongo', dirname(__DIR__) . '/Config/mongo.php');
+
 /**
  * MongoDB 基础类
  *
@@ -20,12 +25,155 @@ namespace Tango\Drive;
  */
 class Mongo {
 
+	/** MongoClient */
+	private static $_oConn;
+
 	/** 数组连接池 */
-	protected static $_lPoolDB = [];
+	protected static $_lPoolConn = [];
+
+	/** 数组连接池配置文件，以此区分不同连接 */
+	protected static $_lPoolConnConf = [];
 
 	/** 内存数组缓存 */
 	protected static $_lPoolData = [];
 
+	/** 内存数组缓存（修改后） */
+	protected static $_lPoolDataChange = [];
+
 	/** _id 的类型，"int" "str" 或者 "bin" */
-	protected $_sKeyType = 'int';
+	private static $_sKeyType = 'int';
+
+	/** _id key */
+	private static $_mKey = '_id';
+
+	/** _id value */
+	protected $_mID;
+
+	/** Mongo 连接名（取自配置文件） */
+	private static $_sConfig;
+
+	/** config */
+	private static $_aConfig;
+
+	/** sharding */
+	private static $_bSharding = FALSE;
+
+	/**
+	 * __construct
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct($mID) {
+
+		if (!static::$_aConfig) {
+			$aConfigFull = Config::get('mongo');
+			$aConfig =& $aConfigFull['server'][static::$_sConfig];
+			if (!is_array($aConfig)) {
+				throw new TangoException('mongo server config "' . static::$_sConfig . '"');
+			}
+			$aConfig += $aConfigFull['default'];
+			static::$_aConfig = $aConfig;
+		}
+
+		switch (static::$_sKeyType) {
+			case 'int':
+				$mID = intval($mID);
+				break;
+			case 'str':
+			case 'bin':
+				$mID = strval($mID);
+				break;
+		}
+
+		$this->_mID = $mID;
+
+		$this->_conn();
+	}
+
+	public function hello() {
+		echo 'config: ' . static::$_sConfig, "\n";
+		$o = $this->_conn();
+		echo ' class: ' . get_class($o), "\n";
+	}
+
+	public function get() {
+
+		$v =& static::$_lPoolDataChange[static::$_sConfig][$this->_mID];
+
+		if (!is_array($v)) {
+
+			$o = $this->_coll();
+
+			$aOrig = $o->findOne([static::$_mKey => $this->_mID]) ?: [];
+			unset($aOrig[static::$_mKey]);
+
+			static::$_lPoolData[static::$_sConfig][$this->_mID] = $aOrig;
+
+			$v = $this->_init($aOrig);
+		}
+
+		return $v;
+	}
+
+	public function update($aUpdate) {
+		$v =& static::$_lPoolDataChange[static::$_sConfig][$this->_mID];
+		if (!is_array($v)) {
+			$v->get();
+		}
+		$v = $aUpdate + $v;
+	}
+
+	public function save() {
+		$v =& static::$_lPoolDataChange[static::$_sConfig][$this->_mID];
+		$o = $this->_coll();
+		return $o->update(
+			[static::$_mKey => $this->_mID],
+			$v,
+			['upsert' => TRUE]
+		);
+	}
+
+	protected function _init($v) {
+		return $v;
+	}
+
+	/**
+	 * _conn
+	 *
+	 * @access protected
+	 * @return \MongoClient
+	 */
+	protected function _conn() {
+
+		if (!static::$_oConn) {
+
+			$aConfig = static::$_aConfig;
+			unset($aConfig['db']);
+			unset($aConfig['collection']);
+			unset($aConfig['debug']);
+			ksort($aConfig);
+
+			$iKey = array_search($aConfig, self::$_lPoolConnConf);
+			if (!is_int($iKey)) {
+				$iKey = count(self::$_lPoolConnConf);
+				self::$_lPoolConnConf[$iKey] = $aConfig;
+
+				self::$_lPoolConn[$iKey] = new \MongoClient();
+			}
+
+			static::$_oConn =& self::$_lPoolConn[$iKey];
+		}
+		return static::$_oConn;
+	}
+
+	protected function _coll() {
+		return $this
+			->_conn()
+			->selectCollection(static::$_aConfig['db'], static::$_aConfig['collection']);
+	}
+
+	protected static function _shading() {
+		return 1;
+	}
 }
