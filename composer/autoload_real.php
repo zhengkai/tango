f605ac411be2b4af8efb0416c56397ccde671a3c
Zheng Kai <zhengkai@gmail.com>
2015-07-06 11:33:45 +0800

    2015-07-06 11:33:45

--- composer/autoload_real.php
+++ composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit7925eaf46d0a8e59559a7e21e144b8d9
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit7925eaf46d0a8e59559a7e21e144b8d9', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
